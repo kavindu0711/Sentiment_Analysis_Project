c71972d78897f6c1c21bfe3adbae4d481dab88f4
kavindu Lakshan <132764900+kavindu0711@users.noreply.github.com>
2026-01-03 17:02:42 +0530

    add loggers

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask,render_template,request,redirect
 from helper import preprocessing,vectorizer,get_prediction
+from logger import logging
 
 app = Flask(__name__)
+
+logging.info('Flask server started...')
 
 data = dict()
 
@@ -15,14 +18,25 @@ def index():
     data['reviews'] = reviews
     data['positive'] = positive
     data['negative'] = negative
+
+    logging.info('-----------Open Home Page-----------')
     return render_template("index.html", data=data)
 
 @app.route("/", methods=['POST'])
 def my_post():
     text = request.form['text']
+    logging.info(f'Text: {text}')
+    
+    
     preprocessed_txt = preprocessing(text)
+    logging.info(f'preprocessed Text: {preprocessed_txt}')
+
+
     vectorized_txt=vectorizer(preprocessed_txt)
+    logging.info(f'Vectorized Text: {vectorized_txt}')
+
     prediction = get_prediction(vectorized_txt)
+    logging.info(f'Prediction: {prediction}')
 
     if prediction == 'negative':
         global negative
